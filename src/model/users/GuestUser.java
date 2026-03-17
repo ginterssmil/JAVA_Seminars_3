88c564308880328b7b3628fbc5a8012e4c91128a
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-03-17 11:59:01 +0200

    Added: model, GuestUser data, getters, setters, constructor, toString

---
+++ src/model/users/GuestUser.java
@@ -0,0 +1,22 @@
+package model.users;
+
+public class GuestUser {
+	private long id;
+
+	private static long counter = 1;
+	
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId() {
+		id = counter++;
+	}
+	
+	public GuestUser() {
+		setId();
+	}
+	
+	
+}
