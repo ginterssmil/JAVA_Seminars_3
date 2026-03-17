91c75b188ef732b076767c9b24a8392220d92e16
Ginters Smiltiņš <s24smilgint@vea.lv>
2026-03-17 11:42:59 +0200

    Added: data, getters setters class Post

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Post {
+import java.sql.Date;
 
+public class Post {
+	String msg;
+	Date date;
+	int countOfLikes;
+	
+	
+	public String getMsg() {
+		return msg;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public int getCountOfLikes() {
+		return countOfLikes();
+	}
+	
+	
+	public void setMsg(String inputMsg) {
+		this.msg = msg;
+	}
+	public void setDate(Date inputDate) {
+		this.date = date;
+	}
+	public void setCountIfLikes(int iputCountOfLikes) {
+		this.countOfLikes = countOfLikes;
+	}
+	
+	
+	
+	
+	
+	
 }
